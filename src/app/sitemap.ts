05ed10e8317446c033a23fae5e43fc04faaa92a5
ridoy <loops.ridoway@gmail.com>
2026-04-01 06:33:23 +0600

    wokw

---
+++ src/app/sitemap.ts
@@ -0,0 +1,29 @@
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = 'https://ridoway.agency'
+
+  // List of all tool routes
+  const toolRoutes = [
+    '',
+    '/tools',
+    '/tools/bkash-charge-calculator-bd',
+    '/tools/nagad-charge-calculator',
+    '/tools/hsc-gpa-calculator-bd',
+    '/tools/ssc-gpa-calculator-bd',
+    '/tools/loan-calculator-bd',
+    '/tools/bangladesh-income-tax-calculator',
+    '/tools/land-area-converter-bd',
+    '/tools/number-to-word-converter-bd',
+    '/tools/age-calculator-bd',
+    '/tools/jpg-to-pdf',
+    '/tools/compress-pdf',
+  ]
+
+  return toolRoutes.map((route) => ({
+    url: `${baseUrl}${route}`,
+    lastModified: new Date(),
+    changeFrequency: route === '' ? 'daily' : 'weekly',
+    priority: route === '' ? 1 : 0.8,
+  }))
+}
